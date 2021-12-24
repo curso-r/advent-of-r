fa43b5f52be21ea37962a772cbe3d6497b927606
C. Lente <7017340+clente@users.noreply.github.com>
2021-12-24 15:58:47 -0300

    Day 24b

---
+++ R/24b_arithmetic_logic_unit.R
@@ -0,0 +1,6 @@
+# As the submarine starts booting up things like the Retro Encabulator, you
+# realize that maybe you don't need all these submarine features after all.
+#
+# What is the smallest model number accepted by MONAD?
+
+# 51147191161261
