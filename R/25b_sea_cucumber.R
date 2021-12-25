ca2a7a41088ed5300a21da1de83aa0ddc19084c8
C. Lente <7017340+clente@users.noreply.github.com>
2021-12-25 11:11:59 -0300

    Day 25a/b

---
+++ R/25b_sea_cucumber.R
@@ -0,0 +1,25 @@
+# Suddenly, the experimental antenna control console lights up:
+#
+# Sleigh keys detected!
+#
+# According to the console, the keys are directly under the submarine. You
+# landed right on them! Using a robotic arm on the submarine, you move the
+# sleigh keys into the airlock.
+#
+# Now, you just need to get them to Santa in time to save Christmas! You check
+# your clock - it is Christmas. There's no way you can get them back to the
+# surface in time.
+#
+# Just as you start to lose hope, you notice a button on the sleigh keys: remote
+# start. You can start the sleigh from the bottom of the ocean! You just need
+# some way to boost the signal from the keys so it actually reaches the sleigh.
+# Good thing the submarine has that experimental antenna! You'll definitely need
+# 50 stars to boost it that far, though.
+#
+# The experimental antenna control console lights up again:
+#
+# Energy source detected.
+# Integrating energy source from device "sleigh keys"...done.
+# Installing device drivers...done.
+# Recalibrating experimental antenna...done.
+# Boost strength due to matching signal phase: 1 star
